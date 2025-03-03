20e79b90baa49f056ca8c615124ea9df86d941f1
Bamidele Adeyiza Promise <bamidelepromise6@gmail.com>
2025-03-03 12:15:00 +0100

    push

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const initializeCronJobs = require("./CronJobs/index"); // Import cron jobs init
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 // Middleware
 app.use(bodyParser.json());
 app.use(express.json());
